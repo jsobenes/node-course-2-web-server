de138fc1ce98947d70fa84153ac49561b15ef4b2
Jorge Sobenes <george@Jorges-MacBook-Pro.local>
2017-08-20 11:19:08 -0300

    Initial Commit

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', hbs);
+
+app.use((req, res, next) => {
+ var now = new Date().toString();
+ var log = `${now}: ${req.method} ${req.url}`;
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('No se pudo grabar el log');
+    }
+  });
+ next(); 
+});
+
+/* app.use((req, res, next) => {
+ res.render('maintenance.hbs') 
+}); */
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+})
+
+/* app.get('/', (req, res) => {
+  //res.send('<h1>Hola Express!</h1>');
+  res.send({
+    name: 'Jorge',
+    likes:[
+      'Biking',
+      'Swiming',
+      'Running'
+    ]
+  })
+}); */
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Hola pagina Home',
+  })  
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  })
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Bad Request'
+  })
+});
+
+app.listen(3000, () => {
+  console.log('Servidor funcionado en puerto 3000');
+});
